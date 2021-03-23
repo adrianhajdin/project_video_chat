e4c3184a3a8dfa73ae66d607c8b7447571789d2b
Adrian Hajdin <adrianhajdin00@gmail.com>
2021-03-23 12:32:59 +0100

    cors

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const io = require("socket.io")(server, {
 	  origin: '*',
 	}
 })
+
+var allowCrossDomain = function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*"); // allow requests from any other server
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); // allow these verbs
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
+}
+	app.use(allowCrossDomain); // plumbing it in as middleware
+
 const cors = require('cors');
 app.use(cors());
 

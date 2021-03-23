a0a67e68ca46584de143d0f3675f5330a020ad51
Adrian Hajdin <adrianhajdin00@gmail.com>
2021-03-23 13:34:02 +0100

    changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const server = require("http").createServer(app);
 
 const io = require("socket.io")(server, {
 	cors: {
-		origin: "http://localhost:3000",
+		origin: "https://priceless-swirles-68e598.netlify.app",
 		methods: [ "GET", "POST" ]
 	}
 });

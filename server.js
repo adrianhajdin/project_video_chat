6e5fde2295b805a976aee25dc6a3b0648266837f
Adrian Hajdin <adrianhajdin00@gmail.com>
2021-03-23 12:25:35 +0100

    cors

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require("express")
 const http = require("http")
 const app = express()
 const server = http.createServer(app)
-const io = require("socket.io")(server)
+const io = require("socket.io")(server, {
+	cors: {
+	  origin: '*',
+	}
+})
 
 io.on("connection", (socket) => {
 	socket.emit("me", socket.id)

d198565b3bdb7c3f51e2e26fa8f1660dd2b5f185
Adrian Hajdin <adrianhajdin00@gmail.com>
2021-03-25 11:49:35 +0100

    cors

--- client/src/Context.js
+++ client/src/Context.js
@@ -4,7 +4,8 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('http://localhost:5000');
+// const socket = io('http://localhost:5000');
+const socket = io('https://warm-wildwood-81069.herokuapp.com/');
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 const io = require("socket.io")(server, {
 	cors: {
-		origin: "http://localhost:3000",
+		origin: "*",
 		methods: [ "GET", "POST" ]
 	}
 });

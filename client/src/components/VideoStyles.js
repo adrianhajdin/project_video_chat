73e7c4e1c9f782be2840e43ec6aa016bf00f28ba
Adrian Hajdin <adrianhajdin00@gmail.com>
2021-03-27 20:26:17 +0100

    test

--- client/src/components/VideoStyles.js
+++ client/src/components/VideoStyles.js
@@ -3,13 +3,13 @@ import { makeStyles } from '@material-ui/core';
 export default makeStyles((theme) => ({
   video: {
     width: '550px',
+    margin: '20px 20px',
     [theme.breakpoints.down('xs')]: {
       width: '300px',
     },
   },
   gridContainer: {
     justifyContent: 'center',
-    alignItems: 'center',
     [theme.breakpoints.down('xs')]: {
       flexDirection: 'column',
     },
